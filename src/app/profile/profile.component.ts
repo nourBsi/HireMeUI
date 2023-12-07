502e3b512b667ec11688582e14be3cd8259a1745
imenbouchriha12 <imenbouchriha5@gmail.com>
2023-12-07 21:27:52 +0100

    image

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -6,24 +6,29 @@ import { Candidat } from '../models/Candidat.model';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css'] // Update with your CSS file if needed
+  styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
   candidat: Candidat = new Candidat();
+  imageSource: string;
+
+  constructor(private config: LoginServiceService) {}
 
   ngOnInit(): void {
     // Fetch user profile data from localStorage
     const storedCandidat = localStorage.getItem('user');
-
+  
     if (storedCandidat) {
-      this.candidat = JSON.parse(storedCandidat); // Corrected the property name to "candidat"
+      this.candidat = JSON.parse(storedCandidat);
+  
+
+      this.imageSource = `${this.candidat.descPhoto},${this.candidat.photoBase64}`;
+      // Vous pouvez également stocker imageSource dans localStorage si nécessaire
+      localStorage.setItem('imageSource', this.imageSource);
     } else {
-      // Handle the case where user data is not found in localStorage
       console.error('User data not found in localStorage');
+     
     }
-    console.log(this.candidat.photoBase64);
-    console.log(this.candidat.descPhoto);
-
-
   }
+
 }
